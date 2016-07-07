bea3da8bab3f6acd9657342c5fad0dcc1aad9253
dushansachinda <dushanabeyruwan@gmail.com>
2016-07-06 23:28:02 -0700

    add

---
+++ RestService2/RestService/RestServiceImpl.svc.cs
@@ -0,0 +1,26 @@
+﻿namespace RestService
+{
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        #region IRestServiceImpl Members
+
+        public string XMLData(string id)
+        {
+            return "You requested product " + id;
+        }
+
+        public string JSONData(string id)
+        {
+            return "You requested product " + id;
+        }
+
+        public string Auth(RequestData rData)
+        {
+            // Call BLL here
+            var data = rData.details.Split('|');
+            return "You requested product " + 111;
+        }
+
+        #endregion
+    }
+}
